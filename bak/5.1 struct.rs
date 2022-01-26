0f2b7d3db5e43cee8b992a76706aa599f2a64c8e
tianye08 <tianye08@kuaishou.com>
2022-01-26 13:13:09 +0800

    struct println

---
+++ bak/5.1 struct.rs
@@ -0,0 +1,26 @@
+struct User {
+  email:String,
+  username:String,
+  active: bool,
+  sign_in_count: u32,
+}
+fn main() {
+  let email = String::from("email");
+  let username = String::from("username");
+
+  let mut user = build_user(email,username);
+  println!("{}{}",user.username, user.email);
+
+  user.username = String::from("username2");
+  println!("{}",user.username)
+  
+}
+
+fn build_user(email: String, username: String) -> User {
+  User {
+      email,
+      username,
+      active: true,
+      sign_in_count: 1,
+  }
+}
